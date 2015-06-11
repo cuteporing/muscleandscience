20b018648a2df96499c9a321d6103fd582daeb02
Krishia Valencia <krishiavalencia@gmail.com>
2015-06-11 21:46:24 +0800

    updated

--- application/controllers/public/news.php
+++ application/controllers/public/news.php
@@ -29,6 +29,11 @@ class news extends pages
 		$this->load->library('pagination');
 	}
 
+	/**
+	 * SHOW ALL BUTTON
+	 * @return
+	 * --------------------------------------------
+	 */
 	static function show_all_btn()
 	{
 		$btn = anchor('news/', 'Show all', array(
@@ -297,7 +302,9 @@ class news extends pages
 	/**
 	 * VIEW COMMENTS
 	 * @param $post_id
-	 * @return $offset
+	 * @param $offset
+	 * @param $comment_count
+	 * @return $container
 	 * --------------------------------------------
 	 */
 	public function view_comments($post_id, $offset = 0, $comment_count = 0)
@@ -333,6 +340,107 @@ class news extends pages
 		return $container;
 	}
 
+	/**
+	 * GET PAGINATION
+	 * @param $view_type
+	 * @return
+	 * --------------------------------------------
+	 */
+	public function get_pagination($view_type)
+	{
+		$config['base_url'] = base_url().'news/'.$view_type.'/';
+		$config['uri_segment'] = 3;
+		$config['total_rows'] = $this->total_rows;
+		$config['per_page'] = $this->per_page;
+		$config['full_tag_open'] = '<ul class="pagination clearfix">';
+		$config['full_tag_close'] = '</ul>';
+		$config['cur_tag_open'] = '<li class="selected"><a>';
+		$config['cur_tag_close'] = '</a><li>';
+		$config['num_tag_open'] = '<li>';
+		$config['num_tag_close'] = '<li>';
+		$config['first_link'] = FALSE;
+		$config['last_link'] = FALSE;
+		$config['prev_link'] = 'previous';
+		$config['next_link'] = 'next';
+		$config['prev_tag_open']  = '<li>';
+		$config['prev_tag_close'] = '</li>';
+		$config['next_tag_open']  = '<li>';
+		$config['next_tag_close'] = '</li>';
+		$this->pagination->initialize($config);
+
+		return $this->pagination->create_links();
+	}
+
+	/**
+	 * IF OFFSET IS MORE THAN THE NO OF NEWS
+	 * @param $total_rows
+	 * @param $offset
+	 * @return $return_value
+	 * --------------------------------------------
+	 */
+	static function cannot_find_page($total_rows, $offset)
+	{
+		$return_value = false;
+		if ( $offset > $total_rows ){
+			$return_value = true;
+		}
+		return $return_value;
+	}
+
+	/**
+	 * GET SEARCH PARAMETER
+	 * @param $other
+	 * @param $other_offset
+	 * --------------------------------------------
+	 */
+	public function get_params($other = "", $other_offset = 0)
+	{
+		$type = str_replace('/', '', $this->uri->slash_segment(2, 'leading'));
+		$page = str_replace('/', '', $this->uri->slash_segment(3, 'leading'));
+
+		(!$page)? $offset = 0 : $offset = $page;
+
+		if($other == "comments"){
+			(!$other_offset)? $offset = 0 : $offset = $other_offset;
+		}
+		else{
+			(!$page)? $offset = 0 : $offset = $page;
+		}
+
+		if( self::cannot_find_page($this->total_rows, $offset) ){
+			return show_404();
+		}
+
+		$this->params = array();
+
+		if($other == "latest"){
+			$this->params['limiter']['limit'] = 2;
+			$this->params['where']    = array("deleted" => 0);
+			$this->params['order_by'] = array("update_datetime" => "desc");
+		}
+		elseif($other == "comments"){
+			$this->params['limiter']['limit'] = $this->comments_limit;
+			$this->params['order_by'] = array(TBL_COMMENTS.".update_datetime" => "desc");
+		}
+		elseif(!$type || $type == "blog"){
+			$this->params['limiter']['limit'] = $this->blog_limit;
+			$this->params['limiter']['offset'] = $offset;
+			$this->params['where']    = array("deleted" => 0);
+			$this->params['order_by'] = array("update_datetime" => "desc");
+		}
+		elseif($type == "post"){
+			$this->params['limiter']['limit'] = $this->post_limit;
+			$this->params['limiter']['offset'] = $offset;
+			$this->params['where']    = array("deleted" => 0);
+			$this->params['order_by'] = array("update_datetime" => "desc");
+		}
+		elseif($type == "title"){
+			$this->params['limiter']['limit'] = $this->post_limit;
+			$this->params['limiter']['offset'] = $offset;
+			$this->params['where']    = array("deleted" => 0, "slug"=>$page);
+			$this->params['order_by'] = array("update_datetime" => "desc");
+		}
+	}
 
 	/**
 	 * GET LATEST NEWS
@@ -387,81 +495,11 @@ class news extends pages
 			$class = array('class'=>'blog clearfix animated fadeIn');
 			$container = element_tag('ul', 'open', $class);
 			$container.= $this->news_list($result, 450);
-			$container.= '</ul>';
+			$container.= element_tag('ul', 'close');
 			$container.= self::show_all_btn();
 		}
 
 		return $container;
-	}
-
-	/**
-	 * IF OFFSET IS MORE THAN THE NO OF NEWS
-	 * @param $total_rows
-	 * @param $offset
-	 * @return $return_value
-	 * --------------------------------------------
-	 */
-	static function cannot_find_page($total_rows, $offset)
-	{
-		$return_value = false;
-		if ( $offset > $total_rows ){
-			$return_value = true;
-		}
-		return $return_value;
-	}
-
-	/**
-	 * GET SEARCH PARAMETER
-	 * @param $is_latest
-	 * --------------------------------------------
-	 */
-	public function get_params($other = "", $other_offset = 0)
-	{
-		$type = str_replace('/', '', $this->uri->slash_segment(2, 'leading'));
-		$page = str_replace('/', '', $this->uri->slash_segment(3, 'leading'));
-
-		(!$page)? $offset = 0 : $offset = $page;
-
-		if($other == "comments"){
-			(!$other_offset)? $offset = 0 : $offset = $other_offset;
-		}
-		else{
-			(!$page)? $offset = 0 : $offset = $page;
-		}
-
-		if( self::cannot_find_page($this->total_rows, $offset) ){
-			return show_404();
-		}
-
-		$this->params = array();
-
-		if($other == "latest"){
-			$this->params['limiter']['limit'] = 2;
-			$this->params['where']    = array("deleted" => 0);
-			$this->params['order_by'] = array("update_datetime" => "desc");
-		}
-		elseif($other == "comments"){
-			$this->params['limiter']['limit'] = $this->comments_limit;
-			$this->params['order_by'] = array(TBL_COMMENTS.".update_datetime" => "desc");
-		}
-		elseif(!$type || $type == "blog"){
-			$this->params['limiter']['limit'] = $this->blog_limit;
-			$this->params['limiter']['offset'] = $offset;
-			$this->params['where']    = array("deleted" => 0);
-			$this->params['order_by'] = array("update_datetime" => "desc");
-		}
-		elseif($type == "post"){
-			$this->params['limiter']['limit'] = $this->post_limit;
-			$this->params['limiter']['offset'] = $offset;
-			$this->params['where']    = array("deleted" => 0);
-			$this->params['order_by'] = array("update_datetime" => "desc");
-		}
-		elseif($type == "title"){
-			$this->params['limiter']['limit'] = $this->post_limit;
-			$this->params['limiter']['offset'] = $offset;
-			$this->params['where']    = array("deleted" => 0, "slug"=>$page);
-			$this->params['order_by'] = array("update_datetime" => "desc");
-		}
 	}
 
 	/**
@@ -477,7 +515,7 @@ class news extends pages
 		$this->total_rows = $this->news_model->get_news_count($this->params);
 		// get parameters
 		$this->get_params();
-
+		// get news
 		$result_post = $this->news_model->get_news($this->params);
 
 		if(!is_null($result_post)){
@@ -512,37 +550,6 @@ class news extends pages
 	}
 
 	/**
-	 * GET PAGINATION
-	 * @param $view_type
-	 * @return
-	 * --------------------------------------------
-	 */
-	public function get_pagination($view_type)
-	{
-		$config['base_url'] = base_url().'news/'.$view_type.'/';
-		$config['uri_segment'] = 3;
-		$config['total_rows'] = $this->total_rows;
-		$config['per_page'] = $this->per_page;
-		$config['full_tag_open'] = '<ul class="pagination clearfix">';
-		$config['full_tag_close'] = '</ul>';
-		$config['cur_tag_open'] = '<li class="selected"><a>';
-		$config['cur_tag_close'] = '</a><li>';
-		$config['num_tag_open'] = '<li>';
-		$config['num_tag_close'] = '<li>';
-		$config['first_link'] = FALSE;
-		$config['last_link'] = FALSE;
-		$config['prev_link'] = 'previous';
-		$config['next_link'] = 'next';
-		$config['prev_tag_open']  = '<li>';
-		$config['prev_tag_close'] = '</li>';
-		$config['next_tag_open']  = '<li>';
-		$config['next_tag_close'] = '</li>';
-		$this->pagination->initialize($config);
-
-		return $this->pagination->create_links();
-	}
-
-	/**
 	 * VIEW NEWS
 	 * @param $page
 	 * @return $container
@@ -550,46 +557,45 @@ class news extends pages
 	 */
 	public function view($page)
 	{
-		$view_type = str_replace('/', '', $this->uri->slash_segment(2, 'leading'));
-
-		$container = "";
-		$result = $this->get_all_news();
+		$view   = str_replace('/', '', $this->uri->slash_segment(2, 'leading'));
 		$class  = array('class'=>'blog clearfix animated fadeIn');
+		$result = $this->get_all_news();
+		$character_limit = 0;
+		$container = "";
 
-		
 		// SEARCH BY BLOG
-		if(!$view_type || $view_type == "blog"){
-			if(count($result) > 0 and !is_null($result)){
-				$container = element_tag('ul', 'open', $class);
-				$container.= $this->news_list($result, 450);
-				$container.= element_tag('ul', 'close');
-			}
-
+		if(!$view || $view == "blog"){
+			$character_limit = 450;
 			$this->per_page = $this->blog_limit;
-			$data['pagination'] = $this->get_pagination($view_type);
+			$data['pagination'] = $this->get_pagination($view);
 		}
 		// SEARCH BY SINGLE POST
-		elseif($view_type == "post"){
+		elseif($view == "post"){
+			$character_limit = 800;
 			if(count($result) > 0 and !is_null($result)){
 				$container = element_tag('ul', 'open', $class);
-				$container.= $this->news_list($result, 450);
+				$container.= $this->news_list($result, 800);
 				$container.= element_tag('ul', 'close');
 			}
-
 			$this->per_page = $this->post_limit;
-			$data['pagination'] = $this->get_pagination($view_type);
+			$data['pagination'] = $this->get_pagination($view);
 		}
 		// SEARCH BY TITLE
-		elseif($view_type == "title"){
-			if(count($result) > 0 and !is_null($result)){
-				$container = element_tag('ul', 'open', $class);
-				$container.= $this->news_list($result);
-				$container.= element_tag('ul', 'close');
+		elseif($view == "title"){
+			$character_limit = 0;
+			$this->per_page = $this->post_limit;
+			if(!is_null($result)){
 				$data['comments'] =  $this->view_comments($result[0]['id'], 0,
 					$result[0]['comment_count']);
 			}
-			$this->per_page = $this->post_limit;
 		}
+
+		if(!is_null($result)){
+			$container = element_tag('ul', 'open', $class);
+			$container.= $this->news_list($result, $character_limit);
+			$container.= element_tag('ul', 'close');
+		}
+
 		$common = new common;
 		$data['breadcrumbs'] = $common->get_breadcrumbs($page);
 		$data['news_list']   = $container;
