a29758e03fe09b79da8c183b089059373bcfcef3
Ashenvale <krishiavalencia@gmail.com>
2015-09-12 09:02:11 +0800

    UPDATED

--- application/controllers/account.php
+++ application/controllers/account.php
@@ -1,20 +1,20 @@
 <?php
-/*********************************************************************************
- ** The contents of this file are subject to the MAS (Muscle and Science)
- * Public License Version 1.0
- * ("License"); You may not use this file except in compliance with the License
- * The Original Code is: MAS (Muscle and Science)
- * The Initial Developer of the Original Code is Krishia Valencia.
- * Portions created by KBVCodes are Copyright (C) KBVCodes.
- * All Rights Reserved.
- *
- ********************************************************************************/
+// /*********************************************************************************
+//  ** The contents of this file are subject to the MAS (Muscle and Science)
+//  * Public License Version 1.0
+//  * ("License"); You may not use this file except in compliance with the License
+//  * The Original Code is: MAS (Muscle and Science)
+//  * The Initial Developer of the Original Code is Krishia Valencia.
+//  * Portions created by KBVCodes are Copyright (C) KBVCodes.
+//  * All Rights Reserved.
+//  *
+//  ********************************************************************************/
 if (! defined ( 'BASEPATH' ))
 	exit ( 'No direct script access allowed' );
 
-require_once (PATH_UTILS.'common.php');
+	require_once( PATH_UTILS.'common.php');
 
-class account extends CI_controller {
+class Account extends CI_controller {
 	public function __construct() {
 		parent::__construct ();
 	}
@@ -29,14 +29,16 @@ class account extends CI_controller {
 		$this->load->view ( TPL_ACCOUNT_TEMPLATES.'header', $data );
 	}
 
-	public function getSidebar() {
-		return $this->load->view ( TPL_ACCOUNT_TEMPLATES.'sidebar' );
-	}
+// 	public function getSidebar() {
+// 		return $this->load->view ( TPL_ACCOUNT_TEMPLATES.'sidebar' );
+// 	}
 
-	public function view($page = 'home') {
+	public function view($page = 'dashboard') {
+
 		if (! file_exists ( APPPATH . "/views/account/{$page}.php" )) {
 			show_404 ();
 		}
+
 		$common = new common ();
 		$common->load_language ();
 
@@ -45,7 +47,18 @@ class account extends CI_controller {
 
 
 		$this->getHeader ( $data );
-		$this->load->view ( 'account/' . $page);
+
+		// Initialize page display
+		switch ($page) {
+			case 'setting':
+				break;
+			default :
+					require_once( PATH_ACCOUNT.'dashboard.php' );
+
+				$dashboard = new Dashboard ();
+				$dashboard->view ( $page );
+				break;
+		}
 	}
 }
-?>
+// ?>
